e10b6af7219d6de485831c6085df04c49b5267ad
Sergey P <s.pansevi4@gmail.com>
2023-07-25 20:40:45 +0300

    feat: add start configuration for in-memory database

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { Album } from 'src/album/entities/album.entity';
+import { Artist } from 'src/artist/entities/artist.entity';
+import { Track } from 'src/track/entities/track.entity';
+import { User } from 'src/user/entities/user.entity';
+import { Favorites } from './interface/favorites.interface';
 
 @Injectable()
-export class DatabaseService {}
+export class DatabaseService {
+  private readonly users: User[] = [];
+  private readonly artists: Artist[] = [];
+  private readonly albums: Album[] = [];
+  private readonly tracks: Track[] = [];
+  private readonly favorites: Favorites = {
+    artists: [],
+    albums: [],
+    tracks: [],
+  };
+}

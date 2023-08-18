e9ffad9c71fdf0342b5e0aa29701acf4fc601c60
Sergey P <s.pansevi4@gmail.com>
2023-08-18 13:29:56 +0300

    feat: add auth guards to the project

---
+++ src/auth/auth-refresh.guard.ts
@@ -0,0 +1,43 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+
+@Injectable()
+export class AuthRefreshGuard implements CanActivate {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromBody(request);
+
+    if (!token) {
+      throw new UnauthorizedException();
+    }
+
+    try {
+      const payload = await this.jwtService.verifyAsync(token, {
+        secret: this.configService.get('JWT_SECRET_REFRESH_KEY'),
+      });
+
+      request['user'] = payload;
+    } catch (error) {
+      throw new UnauthorizedException();
+    }
+
+    return true;
+  }
+
+  private extractTokenFromBody(request: Request): string | undefined {
+    const token = request.body?.refreshToken;
+    return token;
+  }
+}
